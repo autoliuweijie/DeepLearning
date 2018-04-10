a93fe5de9877fb51e3634dcc983c55125f9b1dd5
autoliuweijie <autoliuweijie@163.com>
2018-04-10 23:26:34 +0800

    add mnist_project

---
+++ learning_tensorflow/tensorflow_framework_in_action/c5_mnist_handwriting_recognization/model_save/import_meta_graph.py
@@ -0,0 +1,17 @@
+"""
+    An example model saving
+    @author: Liu Weijie
+    @date: 2018-04-09
+    @reference: <Tensorflow: Deeplearning framework in Action>/Chapter5.4
+"""
+import tensorflow as tf
+
+# importing graph
+saver = tf.train.import_meta_graph("/Users/liuweijie/Desktop/tensorflow_model.ckpt.meta.json")
+
+with tf.Session() as sess:
+
+    # loading variable value to sess
+    saver.restore(sess, "/Users/liuweijie/Desktop/tensorflow_model.ckpt")
+    result = tf.get_default_graph().get_tensor_by_name("add:0")
+    print(sess.run(result))
---
+++ learning_tensorflow/tensorflow_framework_in_action/c5_mnist_handwriting_recognization/model_save/model_restore.py
@@ -0,0 +1,18 @@
+"""
+    An example model saving
+    @author: Liu Weijie
+    @date: 2018-04-09
+    @reference: <Tensorflow: Deeplearning framework in Action>/Chapter5.4
+"""
+import tensorflow as tf
+
+v1 = tf.get_variable("other-v1", shape=[1], initializer=tf.random_normal_initializer(mean=0.0, stddev=1.0), dtype=tf.float32)
+v2 = tf.get_variable("other-v2", shape=[1], initializer=tf.random_normal_initializer(mean=0.0, stddev=1.0), dtype=tf.float32)
+result = v1 + v2
+
+saver = tf.train.Saver({"v1": v1, "v2": v2})
+
+with tf.Session() as sess:
+
+    saver.restore(sess, "/Users/liuweijie/Desktop/tensorflow_model.ckpt")
+    print(sess.run(result))
